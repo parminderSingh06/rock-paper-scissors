c19ed27dd96416d6f9e987167d2a786ecd6ad4bd
Parminder Singh <parmindersingh1086@gmail.com>
2022-12-17 17:17:28 -0800

    Added Multiple Functions and Boilerplate
    HTML file has simple Boilerplate added
    Script file has four functions added
    getComputerChoice and getPlayerChoice obtain the respective choices
    playRound simulations one turn play and game function simulates 5 games

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
+function getComputerChoice()
+{
+    const choices = ["Rock", "Paper", "Scissor"];
+    let random_choice = Math.floor(Math.random() * choices.length);
+    let computer_choice = choices[random_choice].toLowerCase();
+    return computer_choice;
+}
 
+function getPlayerChoice()
+{
+    while(true)
+    {
+        const input = prompt("Choose Rock, Paper, Scissor");
+        let player_choice = input.toLowerCase()
+        if(player_choice == 'rock' || player_choice == 'paper' || player_choice == 'scissor')
+        {
+            return player_choice;
+        }
+        else
+        {
+            alert("Invalid Input! Try Again.");
+        }
+    }
+}
+
+function playRound(player_choice, computer_choice)
+{
+    console.log(`Your Choice: ${player_choice}\nComputer Choice: ${computer_choice}`);
+    if(player_choice == computer_choice)
+    {
+        console.log("Its a Tie");
+        return 0;
+    }
+    else if(player_choice == "rock" && computer_choice == "scissor" || player_choice == "scissor" && computer_choice == "paper" || player_choice == "paper" && computer_choice == "rock")
+    {
+        console.log("You Win");
+        return 1;
+    }
+    else
+    {
+        console.log("You Lost");
+        return 2;
+    }
+
+}
+
+function game()
+{
+    let computer_score = 0;
+    let player_score = 0;
+    for(let i=0;i<5;i++)
+    {
+        console.log(`[Player Score: ${player_score} || Computer Score:${computer_score}]`)
+        let result = playRound(getPlayerChoice(), getComputerChoice());
+        if(result == 1)
+        {
+            player_score++;
+        }
+        else if(result == 2)
+        {
+            computer_score++;
+        }
+    }
+}
+
+game();
